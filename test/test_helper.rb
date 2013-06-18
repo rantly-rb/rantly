ebac5c7cf6223ea0b8ae75e8e92158ee53065aff
Frank Shearar <frank@lshift.net>
2013-06-18 19:15:53 +0100

    Basic coveralls support. Don't force loading of coveralls if it's not already present.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,14 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'simplecov'
 SimpleCov.start
 
+begin
+  # Coveralls is marked as an _optional_ dependency, so don't
+  # throw a fit if it's not there.
+  require 'coveralls'
+  Coveralls.wear!
+rescue LoadError
+end
+
 require 'rantly'
 
 class Test::Unit::TestCase
