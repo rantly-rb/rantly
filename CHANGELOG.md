7812f24a332e80b9d82b634c9ce6c9584ec8f7f9
Eric Bischoff <ebischoff@suse.com>
2015-12-14 14:26:27 +0100

    Version 0.4.0: explore more intensively the tree of failures

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
 # Change Log
-All notable changes to rantly will be documented in this file. The curated log begins at changes to version 0.3.2.
+All notable changes to rantly will be documented in this file. The curated log begins at changes to version 0.4.0.
 
 This project adheres to [Semantic Versioning](http://semver.org/).
+
+## [0.4.0][0.4.0] - 2015-12-14
+### Added
+- Trying harder to shrink collections instead of giving up on first success of property.
+- Added convenience classes Static and Tuple for more control on shrinking
 
 ## [0.3.2][0.3.2] - 2015-09-16
 ### Added
@@ -12,5 +17,6 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 - Improved readability and execution of test suite ([Issue #4][4]).
 - Updates to documentation.
 
+[0.4.0]:    https://github.com/abargnesi/rantly/compare/0.3.2...0.4.0
 [0.3.2]:    https://github.com/abargnesi/rantly/compare/0.3.1...0.3.2
 [4]:        https://github.com/abargnesi/rantly/issues/13
--- lib/rantly/shrinks.rb
+++ lib/rantly/shrinks.rb
@@ -1,11 +1,22 @@
+# Integer : shrink to zero
 class Integer
   def shrink
-    if self < 0 then (self / 2).floor + 1
-    elsif self <= 10 && self > 0 then self - 1
-    elsif self > 0 then ((self + 1) / 2).ceil - 1
+    shrunk = if self > 8
+      self / 2
+    elsif self > 0
+      self - 1
+    elsif self < -8
+      (self + 1) / 2
+    elsif self < 0
+      self + 1
     else
-      return 0
+      0
     end
+    return shrunk
+  end
+
+  def retry?
+    false
   end
 
   def shrinkable?
@@ -13,16 +24,19 @@ class Integer
   end
 end
 
+# String : shrink to ""
 class String
   def shrink
+    shrunk = self.dup
     if self.size > 0
       idx = Random::rand(self.size)
-      shrunk = self.dup
       shrunk[idx] = ""
-      return shrunk
-    else
-      return ""
     end
+    return shrunk
+  end
+
+  def retry?
+    false
   end
 
   def shrinkable?
@@ -30,48 +44,136 @@ class String
   end
 end
 
-class Array
+# Completly unshrinkable array
+class Static
+  def initialize(a)
+    @array = a
+  end
+
+  def [](i)
+    @array[i]
+  end
+
+  def []=(i, value)
+    @array[i] = value
+  end
+
+  def to_s
+    @array.to_s.insert(1, "S ")
+  end
+
+  def inspect
+    self.to_s
+  end
+end
+
+# Array where elements can be shrunk but not removed
+class Tuple
+  def initialize(a)
+    @array = a
+    @position = a.size - 1
+  end
+
+  def [](i)
+    @array[i]
+  end
+
+  def []=(i, value)
+    @array[i] = value
+  end
+
+  def to_s
+    @array.to_s.insert(1, "T ")
+  end
+
+  def inspect
+    self.to_s
+  end
+
   def shrink
-    idx = find_index{|e| e.respond_to?(:shrinkable?) && e.shrinkable?}
-    if idx != nil
-      clone = self.dup
-      clone[idx] = clone[idx].shrink
-      return clone
-    elsif !self.empty?
-      i = Random::rand(self.length)
-      a2 = self.dup
-      a2.delete_at(i)
-      return a2
-    else
-      return self
+    shrunk = @array.dup
+    while @position >= 0
+      e = @array.at(@position)
+      if e.respond_to?(:shrinkable?) && e.shrinkable?
+        break
+      end
+      @position -= 1
     end
+    if @position >= 0
+      shrunk[@position] = e.shrink
+      @position -= 1
+    end
+    return Tuple.new(shrunk)
+  end
+
+  def retry?
+    @position >= 0
   end
 
   def shrinkable?
-    self.any?{|e| e.respond_to?(:shrinkable?) && e.shrinkable? } ||
-      !self.empty?
+    @array.any? {|e| e.respond_to?(:shrinkable?) && e.shrinkable? }
+  end
+end
+
+# Normal array: if elements can't be shrunk, they are removed
+class Array
+  def shrink
+    if (defined? @position).nil?
+      @position = self.length - 1
+    end
+    shrunk = Array.new(self)
+    if @position >= 0
+      e = self.at(@position)
+      if e.respond_to?(:shrinkable?) && e.shrinkable?
+        shrunk[@position] = e.shrink
+      else
+        shrunk.delete_at(@position)
+      end
+      @position -= 1
+    end
+    return shrunk
+  end
+
+  def retry?
+    if (defined? @position).nil?
+      @position = self.length - 1
+    end
+    @position >= 0
+  end
+
+  def shrinkable?
+    !self.empty?
   end
 end
 
 class Hash
   def shrink
-    if self.any?{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? }
-      key,_ = self.detect{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? }
-      clone = self.dup
-      clone[key] = clone[key].shrink
-      return clone
-    elsif !self.empty?
-      key = self.keys.first
-      h2 = self.dup
-      h2.delete(key)
-      return h2
-    else
-      return self
+    if (defined? @position).nil?
+      @position = self.length - 1
     end
+    shrunk = Hash.new(self)
+    keys = shrunk.keys
+    if @position >= 0
+      k = keys.at(@position)
+      e = self[k]
+      if e.respond_to?(:shrinkable?) && e.shrinkable?
+        shrunk[k] = e.shrink
+      else
+        shrunk.delete(k)
+      end
+      @position -= 1
+    end
+    return shrunk
+  end
+
+  def retry?
+    if (defined? @position).nil?
+      @position = self.length - 1
+    end
+    @position >= 0
   end
 
   def shrinkable?
-    self.any?{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? } ||
-      !self.empty?
+    !self.empty?
   end
 end
