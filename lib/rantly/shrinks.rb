8aeb9a450d81eda43adfcb4125178b893e31674c
Eric Bischoff <ebischoff@suse.com>
2015-12-14 14:26:27 +0100

    Version 0.4.0: explore more intensively tree of failures

--- lib/rantly/shrinks.rb
+++ lib/rantly/shrinks.rb
@@ -1,11 +1,18 @@
+# Integer : shrink to zero
 class Integer
-  def shrink
-    if self < 0 then (self / 2).floor + 1
-    elsif self <= 10 && self > 0 then self - 1
-    elsif self > 0 then ((self + 1) / 2).ceil - 1
+  def shrink(position)
+    shrunk = if self > 8
+      self / 2
+    elsif self > 0
+      self - 1
+    elsif self < -8
+      (self + 1) / 2
+    elsif self < 0
+      self + 1
     else
-      return 0
+      0
     end
+    return shrunk, -1
   end
 
   def shrinkable?
@@ -13,16 +20,15 @@ class Integer
   end
 end
 
+# String : shrink to ""
 class String
-  def shrink
+  def shrink(position)
+    shrunk = self.dup
     if self.size > 0
       idx = Random::rand(self.size)
-      shrunk = self.dup
       shrunk[idx] = ""
-      return shrunk
-    else
-      return ""
     end
+    return shrunk, -1
   end
 
   def shrinkable?
@@ -30,48 +36,120 @@ class String
   end
 end
 
-class Array
-  def shrink
-    idx = find_index{|e| e.respond_to?(:shrinkable?) && e.shrinkable?}
-    if idx != nil
-      clone = self.dup
-      clone[idx] = clone[idx].shrink
-      return clone
-    elsif !self.empty?
-      i = Random::rand(self.length)
-      a2 = self.dup
-      a2.delete_at(i)
-      return a2
-    else
-      return self
+# Completly unshrinkable array
+class Static
+  def initialize(a)
+    @array = a
+  end
+
+  def [](i)
+    @array[i]
+  end
+
+  def []=(i, value)
+    @array[i] = value
+  end
+
+  def to_s
+    @array.to_s.insert(1, "S ")
+  end
+
+  def inspect
+    self.to_s
+  end
+end
+
+# Array where elements can be shrunk but not removed
+class Tuple
+  def initialize(a)
+    @array = a
+  end
+
+  def [](i)
+    @array[i]
+  end
+
+  def []=(i, value)
+    @array[i] = value
+  end
+
+  def to_s
+    @array.to_s.insert(1, "T ")
+  end
+
+  def inspect
+    self.to_s
+  end
+
+  def shrink(position)
+    shrunk = @array.dup
+    if position >= @array.length
+      position = @array.length - 1
     end
+    while position >= 0
+      e = @array.at(position)
+      if e.respond_to?(:shrinkable?) && e.shrinkable?
+        break
+      end
+      position -= 1
+    end
+    if position >= 0
+      shrunk[position], p = e.shrink(65535)
+      position -= 1
+    end
+    return Tuple.new(shrunk), position
   end
 
   def shrinkable?
-    self.any?{|e| e.respond_to?(:shrinkable?) && e.shrinkable? } ||
-      !self.empty?
+    @array.any? {|e| e.respond_to?(:shrinkable?) && e.shrinkable? }
+  end
+end
+
+# Normal array: if elements can't be shrunk, they are removed
+class Array
+  def shrink(position)
+    shrunk = self.dup
+    if position >= self.length
+      position = self.length - 1
+    end
+    if position >= 0
+      e = self.at(position)
+      if e.respond_to?(:shrinkable?) && e.shrinkable?
+        shrunk[position], p = e.shrink(65535)
+      else
+        shrunk.delete_at(position)
+      end
+      position -= 1
+    end
+    return shrunk, position
+  end
+
+  def shrinkable?
+    !self.empty?
   end
 end
 
 class Hash
-  def shrink
-    if self.any?{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? }
-      key,_ = self.detect{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? }
-      clone = self.dup
-      clone[key] = clone[key].shrink
-      return clone
-    elsif !self.empty?
-      key = self.keys.first
-      h2 = self.dup
-      h2.delete(key)
-      return h2
-    else
-      return self
+  def shrink(position)
+    shrunk = self.dup
+    keys = shrunk.keys
+    if position >= self.length
+      position = self.length - 1
     end
+    if position >= 0
+      k = keys.at(position)
+      e = self[k]
+      if e.respond_to?(:shrinkable?) && e.shrinkable?
+        shrunk[k], p = e.shrink(65535)
+      else
+        shrunk.delete(k)
+      end
+      position -= 1
+    end
+    return shrunk, position
   end
 
   def shrinkable?
-    self.any?{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? } ||
-      !self.empty?
+    !self.empty?
   end
 end
