0589cd11d95982cfa30e55b462f347434fb04bcc
Anthony Bargnesi <abargnesi@gmail.com>
2016-07-06 14:06:21 -0400

    added back shrink implementation for Hash class
    Originally removed in #10.
    
    Commit b8a12f9aec69dec79365e76aad72872e26859523.

--- lib/rantly/shrinks.rb
+++ lib/rantly/shrinks.rb
@@ -169,3 +169,26 @@ class Deflating
     !@array.empty?
   end
 end
+
+class Hash
+  def shrink
+    if self.any?{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? }
+      key,_ = self.detect{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? }
+      clone = self.dup
+      clone[key] = clone[key].shrink
+      return clone
+    elsif !self.empty?
+      key = self.keys.first
+      h2 = self.dup
+      h2.delete(key)
+      return h2
+    else
+      return self
+    end
+  end
+
+  def shrinkable?
+    self.any?{|_,v| v.respond_to?(:shrinkable?) && v.shrinkable? } ||
+      !self.empty?
+  end
+end
