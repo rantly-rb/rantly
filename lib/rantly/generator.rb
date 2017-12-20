f79d28d6d0754c5b02786f1a6602628eef0edc56
Ana María Martínez Gómez <ammartinez@suse.de>
2017-12-20 16:17:53 +0100

    Allow to add the scale for the normal distribution
    When generating floats using the normal distribution, allow to specify
    the scale. This allow us to decide how close or far away should be the
    generated points from the limit/key point.
    
    Contributes to https://github.com/rantly-rb/rantly/issues/29
    
    Co-authored-by: Víctor Gallego Alcalá <vicgalle@ucm.es>

--- lib/rantly/generator.rb
+++ lib/rantly/generator.rb
@@ -141,12 +141,15 @@ class Rantly
     range(0)
   end
 
-  def float(distribution=nil, center=0)
+  def float(distribution=nil, params={})
     case distribution
     when :normal
+      params[:center] ||= 0
+      params[:scale] ||= 1
+      raise "The distribution scale should be greater than zero" unless params[:scale] > 0
       # Sum of 6 draws from a uniform distribution give as a draw of a normal
       # distribution centered in 3 (central limit theorem).
-      [rand, rand, rand, rand, rand, rand].reduce(0, :+) - 3 + center
+      ([rand, rand, rand, rand, rand, rand].reduce(0, :+) - 3) * params[:scale] + params[:center]
     else
       rand
     end
