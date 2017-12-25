ae539dbd5019dc9c7998d02318c5e264c9c8ba03
Ana María Martínez Gómez <ammartinez@suse.de>
2017-12-25 16:07:39 +0100

    Remove ambiguous `/` warning
    Warning: ambiguous first argument; put parentheses or a space even
    after `/' operator.

--- lib/rantly/generator.rb
+++ lib/rantly/generator.rb
@@ -230,19 +230,19 @@ class Rantly
       end
     end
 
-    ALNUM = Chars.of /[[:alnum:]]/
-    ALPHA = Chars.of /[[:alpha:]]/
-    BLANK = Chars.of /[[:blank:]]/
-    CNTRL = Chars.of /[[:cntrl:]]/
-    DIGIT = Chars.of /[[:digit:]]/
-    GRAPH = Chars.of /[[:graph:]]/
-    LOWER = Chars.of /[[:lower:]]/
-    PRINT = Chars.of /[[:print:]]/
-    PUNCT = Chars.of /[[:punct:]]/
-    SPACE = Chars.of /[[:space:]]/
-    UPPER = Chars.of /[[:upper:]]/
-    XDIGIT = Chars.of /[[:xdigit:]]/
-    ASCII = Chars.of /./
+    ALNUM = Chars.of(/[[:alnum:]]/)
+    ALPHA = Chars.of(/[[:alpha:]]/)
+    BLANK = Chars.of(/[[:blank:]]/)
+    CNTRL = Chars.of(/[[:cntrl:]]/)
+    DIGIT = Chars.of(/[[:digit:]]/)
+    GRAPH = Chars.of(/[[:graph:]]/)
+    LOWER = Chars.of(/[[:lower:]]/)
+    PRINT = Chars.of(/[[:print:]]/)
+    PUNCT = Chars.of(/[[:punct:]]/)
+    SPACE = Chars.of(/[[:space:]]/)
+    UPPER = Chars.of(/[[:upper:]]/)
+    XDIGIT = Chars.of(/[[:xdigit:]]/)
+    ASCII = Chars.of(/./)
 
 
     CLASSES = {
